22dd7e40c926910dae208812f2898bdce76e3629
Jasmine Quach <xjasminequach@gmail.com>
2018-02-16 11:22:52 -0600

    Add first component

---
+++ src/components/StorePicker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <form className="store-selector">
+      </form>
+    )
+  }
+}
+
+export default StorePicker;
