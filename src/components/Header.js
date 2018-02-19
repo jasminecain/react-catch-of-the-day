2dfacfd21dfb8ef0928c47cac9f50ae3d591151f
Jasmine Quach <xjasminequach@gmail.com>
2018-02-19 13:07:19 -0600

    Passing more data into ze props

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
+    console.log(this);
     return (
       <header className="top">
-      <h1>Catch of the Day</h1>
-      <h3 age="5000" cool="true" className="tagline">Fill Me In</h3>
+        <h1>
+          Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+          Day
+        </h1>
+        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
       </header>
     )
   }
