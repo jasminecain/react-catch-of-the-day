d9369c4878f8b2b537f2cdf6cc2562351c1ccb90
Jasmine Quach <xjasminequach@gmail.com>
2018-02-24 21:38:26 -0600

    Add Fish form component

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AddFishForm extends Component {
+  render() {
+    return (
+      <h2>AddFishForm</h2>
+    )
+  }
+}
+
+export default AddFishForm;
