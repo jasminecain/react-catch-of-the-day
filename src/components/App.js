d1d61ea491234593824790be29d71d5c66832dcf
Jasmine Quach <xjasminequach@gmail.com>
2018-02-25 23:07:45 -0600

    Add fishes and order state to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,27 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends Component {
-  com
+  constructor() {
+    super();
+
+    this.addFish = this.addFish.bind(this);
+    // getInitialState
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    // update our state
+    const fishes = {...this.state.fishes};
+    // add in our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // set state
+    this.setState({ fishes });
+    // same as ({ fishes: fishes })
+  }
   render() {
     return (
       <div className="catch-of-the-day">
