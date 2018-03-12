6361fbc18441a6b2e337faf82a598e1b89e75244
Jasmine Quach <xjasminequach@gmail.com>
2018-03-12 16:45:47 -0500

    Update files, createe Fish component, load data to display in state

---
+++ src/components/Fish.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Fish extends Component {
+  render() {
+    return (
+      <li className="menu-fish">
+        <img src={this.props.details.image} alt={this.props.details.name} />
+      </li>
+    )
+  }
+}
+
+export default Fish;
