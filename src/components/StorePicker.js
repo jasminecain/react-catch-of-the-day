24b491ea3e73c29ea39c489f60293ec8a6891f9e
Jasmine Quach <xjasminequach@gmail.com>
2018-03-11 22:56:04 -0500

    Refactor and this binding

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,17 +2,21 @@ import React, { Fragment, Component } from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
-  // constructor() {
-  //   super();
-  //   this.goToStore = this.goToStore.bind(this);
-  // }
-  goToStore(event) {
+  constructor() {
+    super();
+    this.goToStore = this.goToStore.bind(this);
+  }
+
+  myInput = React.createRef();
+
+  goToStore = (event) => {
     event.preventDefault();
     console.log('You Change the URL');
-    // first grab text from the box
+    // first grab text from input
     const storeId = this.storeInput.value;
     console.log(`Going to ${storeId}`);
     // second transition from / to /store/:storeId
+    console.log(this.myInput);
     this.context.router.transitionTo(`/store/${storeId}`)
   }
 
@@ -23,7 +27,13 @@ class StorePicker extends Component {
         <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         {/* {this.goToStore.bind(this)}> */}
           <h2>Please Enter A Store</h2>
-          <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }}/>
+          <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          ref={this.myInput}
+          />
           <button type="submit">Visit Store </button>
         </form>
       </Fragment>
