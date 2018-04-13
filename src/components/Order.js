c7d8811e356c87aa247de6d4c7276a43bd91fdf3
Jasmine Quach <xjasminequach@gmail.com>
2018-04-12 21:56:41 -0500

    add Transitiongroup and CSSTransition tags

--- src/components/Order.js
+++ src/components/Order.js
@@ -32,7 +32,15 @@ class Order extends Component {
       >
         <li key={key}>
           <span>
-            <span>{count}</span>
+            <TransitionGroup component="span" className="count">
+              <CSSTransition
+                classNames="count"
+                key={count}
+                timeout={{ enter: 5000, exit: 5000 }}
+              >
+                <span>{count}</span>
+              </CSSTransition>
+            </TransitionGroup>
             lbs {fish.name}
             {formatPrice(count * fish.price)}
             <button onClick={() => this.props.removeFromOrder(key)}>&times;</button>
